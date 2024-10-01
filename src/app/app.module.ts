64bf23812c5b5c907c059676da3c5c60a19cb20a
lorenz <lorenzcamo93@gmail.com>
2024-10-02 02:37:38 +0800

    add #1 studentlist

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { Component, NgModule } from "@angular/core"
+import { AppComponent } from "./app.component"
+import { RouterModule, Routes } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+import {StudentlistComponent} from "./components/studentlist/studentlist.component";
+
+
+const routes: Routes = [
+  {path: '', component: StudentlistComponent},
+
+]
+
+@NgModule ({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes, {enableTracing: true}),
+    FormsModule,
+  ],
+
+  declarations: [
+    AppComponent,
+    StudentlistComponent,
+  ],
+
+  providers: [
+
+  ],
+
+  bootstrap: [
+    AppComponent
+  ]
+})
+
+export class AppModule {}
---
+++ src/app/components/studentlist/studentlist.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-studentlist',
+  templateUrl: './studentlist.component.html',
+  styleUrls: ['./studentlist.component.css']
+})
+export class StudentlistComponent {
+
+  name: string = '';
+  course: string = '';
+  section: string = '';
+  studentList: string[] = [];
+
+  addItem() {
+    if (this.name.trim() && this.course.trim() && this.section.trim()) {
+      const studentInfo = `Name: ${this.name.trim()} , Course: ${this.course.trim()}, Section: ${this.section.trim()}`;
+      this.studentList.push(studentInfo);
+
+      this.name = '';
+      this.course = '';
+      this.section = '';
+    }
+    else {
+      console.log('Please fill out all fields.');
+    }
+  }
+}
